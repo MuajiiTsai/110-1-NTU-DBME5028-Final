2c10ddf65a0df6afbd26513275070c673379b4dd
MuajiiTsai <92007378+MuajiiTsai@users.noreply.github.com>
2021-12-26 11:22:01 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,24 +9,24 @@ import torchvision.models
 import torch.nn.functional as F 
 from models import SimTriplet
 from torch.utils.data import DataLoader
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from dataset import *
 from sklearn.model_selection import train_test_split
 
 
 parser = argparse.ArgumentParser(description='Train SimTriplet')
-parser.add_argument('--train_dir', default='./train_npy', type=str, help='train data directory')
-parser.add_argument('--test_dir', default='./test_npy', type=str, help='test data directory')
-parser.add_argument('--batch_size', default=64, type=int)
+parser.add_argument('--train_dir', default='./train_npy/', type=str, help='train data directory')
+parser.add_argument('--test_dir', default='./test_npy/', type=str, help='test data directory')
+parser.add_argument('--batch_size', default=16, type=int)
 parser.add_argument('--epoch', default=1, type=int)
-parser.add_argument('--save_model', default='./output', type=str)
 
 args = parser.parse_args()
 train_dir, test_dir = args.train_dir,  args.test_dir
 epochs, batch_size = args.epoch, args.batch_size
-model_path = args.save_model
 
 tools.same_seeds(456101)
+
+output_filename = f'SimTriplet_E{epochs}_B{batch_size}'  
 
 #prepare data
 train_dataset = datasettriplet(img_dir=train_dir, transform=train_transform)
@@ -41,43 +41,48 @@ train_dataset = torch.utils.data.Subset(train_dataset, train_indices)
 valid_dataset = torch.utils.data.Subset(valid_dataset, valid_indices)
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False)
 
 model = SimTriplet()
-# model = model.cuda()
+model = model.cuda()
 
 learning_rate = 1e-3
 best_valid_loss = np.inf
 
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-4)
 
+model_path = f'./output/{output_filename}.pth'
 for num_epoch in range(epochs):
     model.train()
     train_losses = []
     for img1, img2, img3 in tqdm(train_loader):
         optimizer.zero_grad()
-        # img1, img2, img3 = img1.cuda(), img2.cuda(), img3.cuda()
+        img1, img2, img3 = img1.cuda(), img2.cuda(), img3.cuda()
         loss_dict = model(img1, img2, img3)
         loss = loss_dict['loss'].mean()
         loss.backward()
         optimizer.step()
-        # loss = loss.detach().cpu().numpy()
+        loss = loss.detach().cpu().numpy()
         train_losses.append(loss)
+        del img1, img2, img3
     train_loss = np.mean(train_losses)
-    print(f"Training loss: {train_loss}    {num_epoch}/{epochs}")
+    print(f"Training loss: {train_loss}    {num_epoch+1}/{epochs}")
 
     model.eval()
     valid_losses = []
     for img1, img2, img3 in tqdm(valid_loader):
-        # img1, img2, img3 = img1.cuda(), img2.cuda(), img3.cuda()
+        img1, img2, img3 = img1.cuda(), img2.cuda(), img3.cuda()
         loss_dict = model(img1, img2, img3)
         loss = loss_dict['loss'].mean()
-        # loss = loss.detach().cpu().numpy()
+        loss = loss.detach().cpu().numpy()
         valid_losses.append(loss)
+        del img1, img2, img3
     valid_loss = np.mean(valid_losses)
-    print(f"Valid loss: {valid_loss}    {num_epoch}/{epochs}")
+    print(f"Valid loss: {valid_loss}    {num_epoch+1}/{epochs}")
 
     if valid_loss < best_valid_loss:
+        if not os.path.exists('output'):
+            os.mkdir('output')
         best_valid_loss = valid_loss
         torch.save(model.state_dict(), model_path)
         print("The model is saved")
@@ -86,9 +91,8 @@ for num_epoch in range(epochs):
         break
 
 """Finetune"""
-
 #prepare data
-label_dataset = LabelDataset(img_dir=train_dir, transform=valid_transform)
+label_dataset = LabelDataset(annotation_filepath='validation_ground_truth.csv',img_dir=train_dir, transform=valid_transform)
 label_loader = DataLoader(label_dataset, batch_size=1, shuffle=False)
 
 """CHECK THIS"""
@@ -116,6 +120,7 @@ for i in range(50):
                 pred = 0
             if pred == label:
                 correct+=1
+            del img1, img2, feature1, feature2
     grade = correct/len(label_loader)
     print(f"threshold: {thrd}")
     print(f"c-index: {grade}")
@@ -129,7 +134,6 @@ print(f"threshold: {best_thres}, c-index: {best_grade}")
 #prediction
 
 from torchvision.io import read_image, ImageReadMode
-output_filename = f'SimTriplet_E{epochs}_B{batch_size}'    # REMEMBER TO MODIFY
 query_path = f'./queries.csv'
 output_csv = f'./output/{output_filename}.csv'
 
